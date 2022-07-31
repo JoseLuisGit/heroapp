1e61ec1221092dba01a012d4960d0f993ffd4103
JoseLuisGit <joseluisstacruz@gmail.com>
2022-07-31 19:53:50 -0400

    init project

---
+++ src/HeroeApp.jsx
@@ -0,0 +1,7 @@
+
+
+export const HeroeApp = ()=>{
+    return (
+        <h1>HeroeApp</h1>
+    );
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HeroeApp } from './HeroeApp'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HeroeApp />
+  </React.StrictMode>
+)
