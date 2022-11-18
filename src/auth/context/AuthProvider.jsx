88100e0ef08253e5c736ca1e5a8add3fb735f56b
JoseLuisGit <joseluisstacruz@gmail.com>
2022-11-17 21:30:18 -0400

    add login and logout from localstorage and private route {

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -7,26 +7,51 @@ const initState = {
     logged: false
 }
 
+const init = ()=>{
+    const user = JSON.parse( localStorage.getItem('user'));
+
+    return {
+        logged: !!user,
+        user: user
+    }
+
+}
+
 export const AuthProvider = ( {children} )=>{
 
-    const [ authState, dispatch ] = useReducer(authReducer, initState);
+    const [ authState, dispatch ] = useReducer(authReducer, initState, init);
 
     const login = (name = '') =>{
         
+        const user = {
+            id: '23456',
+            name: name
+        };
+        
         const action = {
             type: types.login,
-            payload: {
-                id: '23456',
-                name: name
-            }
+            payload: user
         }
+
+        localStorage.setItem('user', JSON.stringify(user));
+        dispatch(action);
+    }
+
+    const logout = () =>{
+        localStorage.removeItem('user');
+
+        const action = () => {
+            type: types.logout
+        }
+
         dispatch(action);
     }
 
     return (
             <AuthContext.Provider value={ {
                 ...authState,
-                login: login
+                login: login,
+                logout: logout
             } }>
                 { children }
             </AuthContext.Provider>
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { LoginPage } from "../auth/pages/LoginPage";
 import { HeroesRouter } from "../heroes";
+import { PrivateRouter } from "./PrivateRouter";
 
 export const AppRouter = ()=>{
     return (
@@ -8,7 +9,13 @@ export const AppRouter = ()=>{
             <Routes>
                 <Route path="login" element={<LoginPage />} ></Route>
 
-                <Route path="/*" element={<HeroesRouter/>} ></Route>
+                <Route path="/*" element= {
+                    <PrivateRouter>
+                        <HeroesRouter />
+                    </PrivateRouter>
+                }>
+                </Route>
+
             </Routes>
         </>
     );
